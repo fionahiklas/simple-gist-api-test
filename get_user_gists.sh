46987c8381981c8c2d91588c22b96b3fe4730b80
Fiona Bianchi <fiona@hiklas.co.uk>
2022-11-09 20:17:30 +0000

    Updating script

--- get_user_gists.sh
+++ get_user_gists.sh
@@ -16,6 +16,8 @@ done
 
 [ "$USERNAME" = "" ] && echo "Usage: get_users_gists.sh -u <username>" && exit 1
 
+readonly DATE_FILENAME=".gist_date_${USERNAME}"
+
 header_response=$(curl -I "${GISTS_API_BASE_URL}${USERNAME}${GISTS_API_USERS_SUFFIX}" 2> /dev/null)
 found=$(echo "$header_response" | grep "HTTP/2 200")
 
@@ -35,7 +37,23 @@ else
   page_range=$(seq $last_page)  
 fi
 
+# Starts off empty in case we have no timestamp file saved
+since_query_param=""
+
+# Read in timestamp
+if [ -f "$DATE_FILENAME" ]
+then
+  since_timestamp=$(cat $DATE_FILENAME)
+  since_query_param="&since=$since_timestamp"
+  echo "Using since query parameter for filename ${DATE_FILENAME}"
+fi
+
 for page_value in $page_range
 do
-  curl  "${GISTS_API_BASE_URL}${USERNAME}${GISTS_API_USERS_SUFFIX}?page=$page_value" 2> /dev/null | jq -r '. | map([.url, .description] | join(",")) | join("\n")' 
+  curl  "${GISTS_API_BASE_URL}${USERNAME}${GISTS_API_USERS_SUFFIX}?page=${page_value}${since_query_param}" 2> /dev/null | jq -r '. | map([.url, .description] | join(",")) | join("\n")' 
 done   
+
+# Output last timestamp we ran this
+date -Iseconds > $DATE_FILENAME
+echo "Saved since parameter into ${DATE_FILENAME}"
+
